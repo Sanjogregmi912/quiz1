2fb658d746c712ab945b580b258306d5076af618
Sanjogregmi912 <78246727+Sanjogregmi912@users.noreply.github.com>
2021-09-27 11:11:07 +0545

    test added

---
+++ src/test/test_selectdb.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test_selectdb {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
